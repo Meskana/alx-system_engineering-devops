722a682b985f8e07f1a847bb8b582753c4f4525f
root <root@DESKTOP-1GU0KQ2.localdomain>
2024-08-13 17:36:09 +0100

    making it executable

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,25 +4,29 @@ import requests
 """
  function that queries the Reddit API and returns the number of subscribers
 """
+
+
 def number_of_subscribers(subreddit):
-    
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {"User-Agent": "custom_user_agent"}
     try:
-        responds = requests.get(url,headers=headers,allow_redirects=False)
+        responds = requests.get(url, headers=headers, allow_redirects=False)
         if responds.status_code == 200:
             data = responds.json()
             return data['data']['subscribers']
         else:
             return 0
     except Exception as e:
-        return e 
+        return e
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <subreddit>")
         sys.exit(1)
-    
     subreddit = sys.argv[1]
     number_of_subscribers(subreddit)
-    print(f"Number of subscribers in r/{subreddit}: {number_of_subscribers(subreddit)}")
+    print(
+        f"Number of subscribers in r/{subreddit}:"
+        f"{number_of_subscribers(subreddit)}")
