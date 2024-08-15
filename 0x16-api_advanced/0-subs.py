82f5386a29faa55011830f5607f80d82f2fce389
root <root@DESKTOP-1GU0KQ2.localdomain>
2024-08-15 15:34:02 +0100

    advance

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,24 +7,25 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """Return the total nunber of subscriber"""
 
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {"User-Agent": "custom_user_agent"}
-    try:
-        responds = requests.get(url, headers=headers, allow_redirects=False)
-        if responds.status_code == 200:
-            data = responds.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except Exception as e:
-        return e
+    #try:
+    responds = requests.get(url, headers=headers, allow_redirects=False)
+    if responds.status_code == 200:
+        data = responds.json().get('data')
+        return data.get['subscribers']
+    else:
+        return 0
+    #except Exception as e:
+        #return e
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <subreddit>")
-        sys.exit(1)
-    subreddit = sys.argv[1]
-    number_of_subscribers(subreddit)
-    print(f"{number_of_subscribers(subreddit)}")
+#if __name__ == "__main__":
+    #if len(sys.argv) != 2:
+        #print("Usage: python script.py <subreddit>")
+        #sys.exit(1)
+    #subreddit = sys.argv[1]
+    #number_of_subscribers(subreddit)
+    #print(f"{number_of_subscribers(subreddit)}")
