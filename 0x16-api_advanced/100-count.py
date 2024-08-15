d91de21c6ec496dfa9645127cf23e93586a620f7
root <root@DESKTOP-1GU0KQ2.localdomain>
2024-08-15 16:01:53 +0100

    ma

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,6 +7,7 @@ parses the title of all hot articles,
 and prints a sorted count of given keywords
 """
 
+
 def count_words(subreddit, word_list, after=None, count={}):
     """
     recursive function that queries the Reddit API,
@@ -17,10 +18,9 @@ def count_words(subreddit, word_list, after=None, count={}):
     """
     if not word_list or word_list == [] or not subreddit:
         return
-    
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {"User-Agent": "Mozilla/10.0/API"}
-    params = {'limit':100}
+    params = {'limit': 100}
     if after:
         params['after'] = after
     response = requests.get(url,
@@ -32,7 +32,7 @@ def count_words(subreddit, word_list, after=None, count={}):
     data = main_data.get('data')
     children = data.get('children')
     for post in children:
-        title = post.get('data',{}).get('title').lower()
+        title = post.get('data', {}).get('title').lower()
         for word in word_list:
             if word.lower() in title:
                 count[word] = count.get(word, 0) + title.count(word.lower())
@@ -41,13 +41,14 @@ def count_words(subreddit, word_list, after=None, count={}):
         count_words(subreddit, word_list, after, count)
     else:
         sorted_count = sorted(count.items(),
-                              key=lambda x: (-x[1],x[0].lower()))
+                              key=lambda x: (-x[1], x[0].lower()))
         for word, count in sorted_count:
             print(f'{word.lower()}: {count}')
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     if len(sys.argv) < 3:
         print("usage:{}".format(sys.argv[0]))
         print("ex:{}".format(sys.argv[0]))
     else:
-        result = count_words(sys.argv[1],[x for x in sys.argv[2].split()])
-
+        result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
